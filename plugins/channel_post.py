b885bd7dcabb9881fd673437e0401dccdd54549d
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-23 22:45:51 +0600

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,10 +4,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, DB_CHANNEL, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, DB_CHANNEL, DISABLE_CHANNEL_BUTTON, OWNER_ID
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
+@Bot.on_message(filters.private & filters.user(OWNER_ID) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
