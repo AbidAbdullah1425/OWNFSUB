39232a45ef34c984529c62acf171fc487fee19da
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-01-13 20:48:33 +0600

    Update FSUB.py

--- plugins/FSUB.py
+++ plugins/FSUB.py
@@ -8,30 +8,28 @@ from pyrogram import Client, filters
 @Bot.on_message(filters.private & filters.user(OWNER_ID) & filters.command("update"))
 async def update_fsubs(client, message):
     update_fsub_values()
-    await message.reply("Updated Succesfully")
+    await message.reply("Updated Successfully")
 
 
-#update fsub_1 value using commands
-def update_fsub1_value():
-    new_fsub_value = input("Send new FSUB_1 Channel id")
+# Update fsub_1 value using commands
+def update_fsub1_value(new_fsub_value):
     if not new_fsub_value.startswith("-100"):
-            print("Invaild fsub1 channel must starts with -100")
-            return
+        return "Invalid fsub1 channel, it must start with -100"
 
     result = collection.update_one(
-    {"_id": "6784b63b7966c6407562bb40"},
-    {"$set": {"FSUB_1": new_fsub_value}}
+        {"_id": "6784b63b7966c6407562bb40"},
+        {"$set": {"FSUB_1": new_fsub_value}}
     )
 
     if result.matched_count > 0:
-            print("FSUB_1 updated")
+        return "FSUB_1 updated"
     else:
-            print("Document not found or update unsuccesfull")
+        return "Document not found or update unsuccessful"
 
 @Bot.on_message(filters.private & filters.user(OWNER_ID) & filters.command("set_fsub1"))
 async def setfsub1(client, message):
-    if len(message.command) > 2:
-        await LOGGER.error("format problem example: /set_fsub1 -100828292922")
+    if len(message.command) < 2:
+        await message.reply("Please provide a valid FSUB_1 channel ID, example: /set_fsub1 -100828292922")
         return
 
     new_fsub_value = message.command[1]
