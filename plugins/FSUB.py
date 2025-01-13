0334c20216c8dadd5c09fe81c0e9449868a4f49f
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-01-13 19:22:26 +0600

    Update FSUB.py

--- plugins/FSUB.py
+++ plugins/FSUB.py
@@ -15,18 +15,18 @@ async def update_fsubs(client, message):
 def update_fsub1_value():
     new_fsub_value = input("Send new FSUB_1 Channel id")
     if not new_fsub_value.startswith("-100"):
-    	print("Invaild fsub1 channel must starts with -100")
-    	return
-    	
+            print("Invaild fsub1 channel must starts with -100")
+            return
+
     result = collection.update_one(
     {"_id": "6784b63b7966c6407562bb40"},
     {"$set": {"FSUB_1": new_fsub_value}}
     )
-    
+
     if result.matched_count > 0:
-    	print("FSUB_1 updated")
+            print("FSUB_1 updated")
     else:
-    	print("Document not found or update unsuccesfull")
+            print("Document not found or update unsuccesfull")
 
 @Bot.on_message(filters.private & filters.user(OWNER_ID) & filters.command("set_fsub1"))
 async def setfsub1(client, message)
@@ -37,4 +37,3 @@ async def setfsub1(client, message)
     new_fsub_value = message.command[1]
     response = update_fsub1_value(new_fsub_value)
     await message.reply(response)
-    
