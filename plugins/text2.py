881b6e6839d89e02b5db9a7df6a9ab5aeb39657d
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-13 13:55:36 +0600

    Update text2.py

--- plugins/text2.py
+++ plugins/text2.py
@@ -1,1 +1,6 @@
-from test import VAR
+from text import VAR
+from bot import Bot
+
+@Bot.on_message(filters.command("checkvar") & filters.user(OWNER_ID))
+async def get_var(client, message):
+    await message.reply_text(f"VAR: {VAR}")
