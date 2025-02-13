9a5f1c80249b8fc8bf923e2a8f102500d4c75018
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-13 18:24:09 +0600

    Update text2.py

--- plugins/text2.py
+++ plugins/text2.py
@@ -1,5 +1,7 @@
-from text import VAR
+from plugins.text import VAR  # Import from the correct path
 from bot import Bot
+from pyrogram import filters
+from config import OWNER_ID
 
 @Bot.on_message(filters.command("checkvar") & filters.user(OWNER_ID))
 async def get_var(client, message):
