25b2e95d2fc0880600b69c90d020a02198c49727
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-13 13:05:04 +0600

    Update plugins

---
+++ plugins/text.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+import os
+from bot import Bot
+
+VAR = int(os.environ.get('VAR', 10))  # Load VAR from environment
+
+@Bot.on_message(filters.command("var") & filters.user(OWNER_ID))
+async def update_var(client, message):
+    global VAR
+    try:
+        new_value = int(message.text.split()[1])
+        os.environ['VAR'] = str(new_value)  # Update the environment variable
+        VAR = new_value  # Update the in-memory variable
+        await message.reply_text(f"VAR updated to {VAR}")
+    except (IndexError, ValueError):
+        await message.reply_text("Usage: /var <number>")
