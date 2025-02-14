2c363567a43d3549ce1569d3b283553c956257df
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-14 22:39:42 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,7 +83,7 @@ class Bot(Client):
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
                 sys.exit()
         try:
-            db_channel = await self.get_chat(CHANNEL_ID)
+            db_channel = await self.get_chat(DB_CHANNEL)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
