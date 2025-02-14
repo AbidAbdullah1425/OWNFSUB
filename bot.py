82c5947b85699c4511ee15a5589fdcd79b05a62c
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-14 12:58:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -105,13 +105,10 @@ FSUB_4: {FSUB_4}
 self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/NocoFlux")
 self.LOGGER(__name__).info(f"""Bot is Good""")
 
-        self.username = usr_bot_me.username
-        #web-response
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+self.username = usr_bot_me.username  # Fixed indentation
 
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+# Web-response
+app = web.AppRunner(await web_server())
+await app.setup()
+bind_address = "0.0.0.0"
+await web.TCPSite(app, bind_address, PORT).start()
