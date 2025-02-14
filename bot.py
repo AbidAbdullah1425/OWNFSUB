f1c129cf6f342208a95f8d968cbb00edc4c11faa
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-14 11:47:06 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,8 +94,17 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/NocoFlux")
-        self.LOGGER(__name__).info(f"""Bot is Good""")
+
+self.LOGGER(__name__).info(f"""[Force Sub Channels]
+FSUB_1: {FSUB_1}
+FSUB_2: {FSUB_2}
+FSUB_3: {FSUB_3}
+FSUB_4: {FSUB_4}
+""")
+
+self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/NocoFlux")
+self.LOGGER(__name__).info(f"""Bot is Good""")
+
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
